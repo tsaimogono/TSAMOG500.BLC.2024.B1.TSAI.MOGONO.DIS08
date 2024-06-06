8e107cf3b8300e345ce23520d3e56151c9ef5ddb
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-06-06 13:29:17 +0200

    created a van folder and moved vans,jsx and vasdetail to the folder

--- index.jsx
+++ index.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Vans from "./pages/Vans"
-import VanDetail from "./pages/VanDetail"
+import Vans from "./Van/Vans"
+import VanDetail from "./Van/VanDetail"
 import Layout from "./components/Layout"
 
 import "./server"

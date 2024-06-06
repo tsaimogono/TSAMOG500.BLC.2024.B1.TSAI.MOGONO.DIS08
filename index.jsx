ca11cd0153343389e2331dbcaac25910f4021ae8
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-06-06 11:46:20 +0200

    created a component fil,modified the header and layout.jsx. and imported the header

--- index.jsx
+++ index.jsx
@@ -4,23 +4,19 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
+import VanDetail from "./pages/VanDetail"
 
 import "./server"
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   )
